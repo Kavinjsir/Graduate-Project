0720df60d99ef7cee34de3f6ff0370e79bff5c4b
Jarvan <tonyking_jty@126.com>
2018-05-02 20:37:49 +0800

    basic home page with auto slide

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import Carousel from 'nuka-carousel';
+import { Card, Image, Segment } from 'semantic-ui-react';
 
 export default class HomePage extends React.Component {
   render() {
+    const ImgList = [
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide1",
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide2",
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide3",
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide4",
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide5",
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"
+    ]
     return (
-      <Grid>
-        <Grid.Row>主页</Grid.Row>
-      </Grid>
+      <div className="homepage">
+      <Carousel clas autoplay={true}>
+        {/* {
+          ImgList.map(source => (
+            <img src={source} />
+          ))
+        } */}
+        {
+                    ImgList.map(source => (
+                      <Segment>
+                      <Card centered>
+                        <Card.Content>
+                          <Image floated='right' src={source} size='huge' />
+                          <Card.Header>
+                            source
+                          </Card.Header>
+                          <Card.Meta>
+                            {(new Date()).toLocaleDateString()}
+                          </Card.Meta>
+                          <Card.Description>
+                            Need for Details
+                          </Card.Description>
+                        </Card.Content>
+                      </Card>
+                      </Segment>
+                    ))
+        }
+      </Carousel>
+      </div>
     );
   }
 }
