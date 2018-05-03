93c6bf68e619d900a715c3b5b28ae0d0a106bc3c
Jarvan <tonyking_jty@126.com>
2018-05-03 16:33:05 +0800

    basic ui for home page

--- src/components/home.js
+++ src/components/home.js
@@ -7,36 +7,80 @@ export default class HomePage extends React.Component {
     const ImgList = [
       "http://placehold.it/1000x400/ffffff/c0392b/&text=slide1",
       "http://placehold.it/1000x400/ffffff/c0392b/&text=slide2",
-      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide3",
+      "http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"
     ]
     return (
       <div className="homepage">
-      <Carousel
-        autoplay={true}
-        dragging={true}
-        renderCenterLeftControls={null}
-        renderCenterRightControls={null}
-        cellSpacing={20}
-      >
-        {
-                    ImgList.map(source => (
-                      <Card color='blue' fluid centered>
-                        <Card.Content>
-                          <Image floated='right' src={source} size='massive' />
-                          <Card.Header textAlign='right'>
-                            source
-                          </Card.Header>
-                          <Card.Meta textAlign='right'>
-                            {(new Date()).toLocaleDateString()}
-                          </Card.Meta>
-                          <Card.Description textAlign='center'>
-                            Need for Details
-                          </Card.Description>
-                        </Card.Content>
-                      </Card>
-                    ))
-        }
-      </Carousel>
+        <Carousel
+          autoplay={true}
+          dragging={true}
+          renderCenterLeftControls={null}
+          renderCenterRightControls={null}
+          cellSpacing={20}
+          vertical={true}
+          wrapAround={true}
+        >
+          <Card fluid centered>
+            <Card.Content>
+              <Image floated='right' src={ImgList[0]} size='massive' />
+              <Card.Header textAlign='right'>
+                <h1>文本挖掘系统</h1>
+              </Card.Header>
+              <Card.Meta textAlign='right'>
+                <hr />
+                一个服务于企业的信息系统，基于『Spark大数据架构』和『自然语言处理算法』的文本挖掘解决方案。<br />
+                为使用者提供用于文本分析及挖掘的核心工具，帮助用户更好的处理文本分析需求。
+              </Card.Meta>
+            </Card.Content>
+          </Card>
+
+          <Card fluid centered>
+            <Card.Content>
+              <Image floated='right' src={ImgList[1]} size='massive' />
+              <Card.Header textAlign='right'>
+                <h1>邮件监管模块</h1>
+              </Card.Header>
+              <Card.Meta textAlign='right'>
+                <hr />
+                利用大数据处理技术，将邮件作为文本信息载体，与机器学习等方法有机结合。<br />
+                提供基于若干机器学习方法的垃圾邮件分类、涉密邮件识别、敏感信息审查模块。
+              </Card.Meta>
+            </Card.Content>
+          </Card>
+
+          <Card fluid centered>
+            <Card.Content>
+              <Image floated='right' src={ImgList[2]} size='massive' />
+              <Card.Header textAlign='right'>
+                <h1>信息抽取模块</h1>
+              </Card.Header>
+              <Card.Meta textAlign='right'>
+                <hr />
+                基于若干分词技术与机器学习方法的结合，依托spark分析平台<br />
+                提供了基于文本的个人标签信息抽取的服务
+              </Card.Meta>
+            </Card.Content>
+          </Card>
+
+          <Card fluid centered>
+            <Card.Content>
+              <Image floated='right' src={ImgList[3]} size='massive' />
+              <Card.Header textAlign='right'>
+                <h1>算法管理</h1>
+              </Card.Header>
+              <Card.Meta textAlign='right'>
+                <hr />
+                可视化管理平台，支持：<br />
+                查看当前算法<br />
+                浏览可用算法<br />
+                替换当前算法<br />
+                升级指定算法<br />
+              </Card.Meta>
+            </Card.Content>
+          </Card>
+
+        </Carousel>
       </div>
     );
   }
